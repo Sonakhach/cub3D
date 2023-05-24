5054dd768a0bdecdbc3457d5767a143de24f3654
Sona Khachatryan <sokhacha@c2r1s4.42yerevan.am>
2023-05-24 17:22:20 +0400

    finel finish:)))

--- src/print_error.c
+++ src/print_error.c
@@ -15,7 +15,6 @@
 void	print_close(t_addres *address, char *str)
 {
 	write(1, str, ft_strlen(str));
-	(void)address;
 	free_all(address);
 }
 
@@ -23,18 +22,6 @@ int	close_game(t_addres *address)
 {
 	print_close(address, "Game closed\n");
 	exit(0);
-}
-
-void	print_error(t_addres *address, char *err, int flag)
-{
-	write(2, "Error!\n", 7);
-	write(2, err, ft_strlen(err));
-	write(2, "\n", 1);
-	if (flag == 1)
-		free_all(address);
-	(void)flag;
-	(void)address;
-	exit(EXIT_FAILURE);
 }
 
 void	free_all(t_addres *address)
